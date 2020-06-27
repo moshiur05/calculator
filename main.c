d6c65668df4a7eebf8ea86587de28a3e0d2724f8
moshiur <moshiur71199@gmail.com>
2020-06-27 21:32:10 +0600

    modified main.c

--- main.c
+++ main.c
@@ -5,29 +5,57 @@
 
 enum {real_, complex_} number_system;
 
-void push_real(double);
-double pop_real(void);
-struct
+enum action_
 {
-	double *ptr;
-	int size;
-	void (*push)(double);
-	double (*pop)(void);
-} calculation_stack_real = {NULL, 0, push_real, pop_real};
+	EXIT,
+	NUMBER,
+	ADD,
+	SUBSTRACT,
+	MULTIPLY,
+	DIVIDE,
+	EXP,
+	LN,
+	POW,
+	LOG,
+	COS,
+	SIN,
+	TAN,
+	SEC,
+	COSEC,
+	COT,
+	ACOS,
+	ASIN,
+	ATAN,
+	ASEC,
+	ACOSEC,
+	ACOT
+} *action = NULL;
 
-void push_complex(complex double);
-complex double pop_complex(void);
+void push_(void *);
+void pop_(void);
 struct
 {
-	complex double *ptr;
+	void *ptr;
 	int size;
-	void (*push)(complex double);
-	complex double (*pop)(void);
-} calculation_stack_complex = {NULL, 0, push_complex, pop_complex};
+	void (*push)(void *);
+	void (*pop)(void);
+} calculation_stack = {NULL, 0, push_, pop_};
+
+struct
+{
+	void *ptr;
+	int index;
+} num_stack = {NULL, 0};
 
 int choose_number_system(char *);
 int is_equal_str(const char *str1, const char *str2);
 char *get_line(void);
+size_t get_num_stack_size(char *);
+int get_num_action(char *);
+void get_num_stack(char *);
+void get_action(char *);
+void get_result(void);
+void free_memory(void);
 
 int main(int argc, char **argv)
 {
@@ -36,6 +64,12 @@ int main(int argc, char **argv)
 	while(!is_equal_str((input = get_line()), "exit"))
 	{
 		if(choose_number_system(input) == EXIT_SUCCESS){continue;}
+		num_stack.ptr = malloc(get_num_stack_size(input));
+		action = (enum action_ *)malloc(get_num_action(input)*sizeof(enum action_));
+		get_num_stack(input);
+		get_action(input);
+		get_result();
+		free_memory();
 	}
 
 	return 0;
@@ -56,32 +90,30 @@ int choose_number_system(char *str)
 	return EXIT_SUCCESS;
 }
 
-void push_real(double var)
+void push_(void *numptr)
 {
-	calculation_stack_real.ptr = (double *)realloc(calculation_stack_real.ptr, ++calculation_stack_real.size);
-	calculation_stack_real.ptr[calculation_stack_real.size - 1] = var;
+	if(number_system == real_)
+	{
+		calculation_stack.ptr = realloc(calculation_stack.ptr, ++calculation_stack.size*(sizeof(double)));
+		((double *)calculation_stack.ptr)[calculation_stack.size - 1] = *((double *)numptr);
+	} else if(number_system == complex_)
+	{
+		calculation_stack.ptr = realloc(calculation_stack.ptr, ++calculation_stack.size*(sizeof(complex double)));
+		((complex double *)calculation_stack.ptr)[calculation_stack.size - 1] = *((complex double *)numptr);
+	}
 	return;
 }
 
-double pop_real(void)
+void pop(void)
 {
-	double var = calculation_stack_real.ptr[--calculation_stack_real.size];
-	calculation_stack_real.ptr = (double *)realloc(calculation_stack_real.ptr, calculation_stack_real.size);
-	return var;
-}
-
-void push_complex(complex double var)
-{
-	calculation_stack_complex.ptr = (complex double *)realloc(calculation_stack_complex.ptr, ++calculation_stack_complex.size);
-	calculation_stack_complex.ptr[calculation_stack_complex.size - 1] = var;
+	if(number_system == real_)
+	{
+		calculation_stack.ptr = realloc(calculation_stack.ptr, --calculation_stack.size*(sizeof(double)));
+	} else if(number_system == complex_)
+	{
+		calculation_stack.ptr = realloc(calculation_stack.ptr, --calculation_stack.size*(sizeof(complex double)));
+	}
 	return;
-}
-
-complex double pop_complex(void)
-{
-	complex double var = calculation_stack_complex.ptr[--calculation_stack_complex.size];
-	calculation_stack_complex.ptr = (complex double *)realloc(calculation_stack_complex.ptr, calculation_stack_complex.size);
-	return var;
 }
 
 int is_equal_str(const char *str1, const char *str2)
@@ -99,7 +131,7 @@ char *get_line(void)
 	static char *str = NULL;
 	free(str);
 	int size = 1;
-	str = malloc(size);
+	str = (char *)malloc(size);
 	printf("=> ");
 	while((str[size - 1] = getchar()) != '\n')
 	{
@@ -107,4 +139,14 @@ char *get_line(void)
 	}
 	str[size - 1] = '\0';
 	return str;
+}
+
+void free_memory(void)
+{
+	free(num_stack.ptr);
+	num_stack.ptr = NULL;
+	free((void *)action);
+	action = NULL;
+	free(calculation_stack.ptr);
+	calculation_stack.ptr = NULL;
 }
